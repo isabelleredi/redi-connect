323a97d76ae3b33ddc478b2193b4a5c441fa547a
christos <paschalidi@outlook.com>
2019-07-23 22:57:44 +0200

    service http

--- redi-connect-front/src/services/http/http.tsx
+++ redi-connect-front/src/services/http/http.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import axios from 'axios';
-import _ from 'lodash';
-
-import { history } from '../history/history';
-import { getAccessToken, isLoggedIn, purgeAllSessionData } from '../auth/auth';
+import React from "react";
+import axios from "axios";
+import _ from "lodash";
+import { history } from "../history/history";
+import { getAccessToken, isLoggedIn, purgeAllSessionData } from "../auth/auth";
 
 export const nonLoggedInHttp = axios.create();
 
@@ -11,7 +10,7 @@ export const http = axios.create();
 
 http.interceptors.request.use(
   function(config) {
-    const isAuthorizationHeaderSet = _.has(config, 'headers.Authorization');
+    const isAuthorizationHeaderSet = _.has(config, "headers.Authorization");
     const _isLoggedIn = isLoggedIn();
     if (_isLoggedIn && !isAuthorizationHeaderSet) {
       const accessToken = getAccessToken();
@@ -31,32 +30,21 @@ http.interceptors.response.use(
     if (err.response) {
       err.userMessage = err.response.data.error.message;
     } else if (err.request) {
-      err.userMessage = 'Please check your internet connection.';
+      err.userMessage = "Please check your internet connection.";
     } else {
-      err.userMessage = 'An error occurred; please try again.';
+      err.userMessage = "An error occurred; please try again.";
     }
     console.log(err);
-    
+
     if (_.includes([401, 403], err.response.status)) {
-      purgeAllSessionData()
-      history.push(`/front/login?goto=${encodeURIComponent(history.location.pathname)}`);
+      purgeAllSessionData();
+      history.push(
+        `/front/login?goto=${encodeURIComponent(history.location.pathname)}`
+      );
     } else {
-      history.push('/error/4xx');
+      history.push("/error/4xx");
     }
-    
   }
 );
 
 export const HttpContext = React.createContext(http);
-
-/*
-export function with$Http(Component) {
-  return function ComponentWith$Http(props) {
-    return (
-      <$HttpContext.Consumer>
-        {_$http => <Component {...props} $http={_$http} /> }
-      </$HttpContext.Consumer>
-    );
-  };
-}
-*/
