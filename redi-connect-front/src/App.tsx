fcbadb776367c2c734ec8dc85d23606c8f30d5b2
christos <paschalidi@outlook.com>
2019-07-23 23:08:25 +0200

    lints App

--- redi-connect-front/src/App.tsx
+++ redi-connect-front/src/App.tsx
@@ -1,66 +1,59 @@
-import React, { useEffect } from 'react';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import React, { useEffect } from "react";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { Provider as StoreProvider } from "react-redux";
+import { history, Router } from "./services/history/history";
+import { Routes } from "./components/Routes";
+import { store } from "./redux/store";
+import { profileFetchStart } from "./redux/user/actions";
 
-import { Provider as StoreProvider } from 'react-redux';
-import { rootReducer } from './redux/reducers';
-
-import { rootEpic } from './redux/epics';
-import logo from './logo.svg';
-import './App.css';
-import { history, Router } from './services/history/history';
-import { Routes } from './components/Routes';
-import { store } from './redux/store';
-import { UserActionType } from './redux/user/types';
-import { profileFetchStart } from './redux/user/actions';
-
-const mainColour = '#58adc4';
+const mainColour = "#58adc4";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: mainColour,
-      contrastText: '#fff',
+      contrastText: "#fff"
     },
     secondary: {
-      main: '#ea5b25',
+      main: "#ea5b25"
     },
     error: {
-      main: '#b00020',
-    },
+      main: "#b00020"
+    }
   },
   typography: {
-    fontFamily: 'Roboto',
+    fontFamily: "Roboto"
   },
   overrides: {
     MuiStepIcon: {
       root: {
-        color: '#eaeaea',
-        '&$active': {
-          color: '#ea5b25',
+        color: "#eaeaea",
+        "&$active": {
+          color: "#ea5b25"
         },
-        '&$completed': {
-          color: '#ea5b25',
-        },
-      },
+        "&$completed": {
+          color: "#ea5b25"
+        }
+      }
     },
     MuiStepLabel: {
       label: {
-        color: '#eaeaea',
-        '&$active': {
-          color: '#ea5b25',
+        color: "#eaeaea",
+        "&$active": {
+          color: "#ea5b25"
         },
-        '&$completed': {
-          color: '#ea5b25',
-        },
-      },
-    },
-  },
+        "&$completed": {
+          color: "#ea5b25"
+        }
+      }
+    }
+  }
 });
 
 const App = () => {
   useEffect(() => {
-    store.dispatch(profileFetchStart())
-  }, [])
+    store.dispatch(profileFetchStart());
+  }, []);
   return (
     <MuiThemeProvider theme={theme}>
       <StoreProvider store={store}>
