a9a898195640bf96ccedad634e44cf6b2803cfa2
christos <paschalidi@outlook.com>
2019-07-23 22:57:59 +0200

    service auth

--- redi-connect-front/src/services/auth/auth.ts
+++ redi-connect-front/src/services/auth/auth.ts
@@ -1,10 +1,10 @@
-import { RedUser } from '../../types/RedUser';
-import { AccessToken } from '../../types/AccessToken';
-import { RedProfile } from '../../types/RedProfile';
+import { RedUser } from "../../types/RedUser";
+import { AccessToken } from "../../types/AccessToken";
+import { RedProfile } from "../../types/RedProfile";
 
 export const isLoggedIn = (): boolean => {
-  const profile: any = window.localStorage.getItem('redProfile');
-  const accessToken: any = window.localStorage.getItem('accessToken');
+  const profile: any = window.localStorage.getItem("redProfile");
+  const accessToken: any = window.localStorage.getItem("accessToken");
   try {
     const r1: any = JSON.parse(profile);
     const r2: any = JSON.parse(accessToken);
@@ -15,24 +15,24 @@ export const isLoggedIn = (): boolean => {
 };
 
 export const getRedUser = (): RedUser =>
-  JSON.parse(<string>window.localStorage.getItem('redUser'));
+  JSON.parse(<string>window.localStorage.getItem("redUser"));
 
 export const saveRedUser = (redUser: RedUser) => {
-  window.localStorage.setItem('redUser', JSON.stringify(redUser));
+  window.localStorage.setItem("redUser", JSON.stringify(redUser));
 };
 
 export const getRedProfile = (): RedProfile =>
-  JSON.parse(<string>window.localStorage.getItem('redProfile'));
+  JSON.parse(<string>window.localStorage.getItem("redProfile"));
 
 export const saveRedProfile = (redProfile: RedProfile) => {
-  window.localStorage.setItem('redProfile', JSON.stringify(redProfile));
+  window.localStorage.setItem("redProfile", JSON.stringify(redProfile));
 };
 
 export const getAccessToken = (): AccessToken =>
-  JSON.parse(<string>window.localStorage.getItem('accessToken'));
+  JSON.parse(<string>window.localStorage.getItem("accessToken"));
 
 export const saveAccessToken = (accessToken: AccessToken) => {
-  window.localStorage.setItem('accessToken', JSON.stringify(accessToken));
+  window.localStorage.setItem("accessToken", JSON.stringify(accessToken));
 };
 
 export const purgeAllSessionData = () => {
