b39480264869b1047bad059b0d392d84a944da9d
christos <paschalidi@outlook.com>
2019-07-23 22:53:27 +0200

    lints redux profiles

--- redi-connect-front/src/redux/profiles/epics.ts
+++ redi-connect-front/src/redux/profiles/epics.ts
@@ -1,10 +1,10 @@
-import { ActionsObservable, ofType } from 'redux-observable';
-import { map, switchMap, tap, catchError } from 'rxjs/operators';
-import { from, concat, of } from 'rxjs';
-import { API_URL } from '../../config/config';
-import { http } from '../../services/http/http';
-import { profilesFetchOneSuccess, profilesFetchOneStart } from './actions';
-import { ProfilesActions, ProfilesActionType } from './types';
+import { ActionsObservable, ofType } from "redux-observable";
+import { map, switchMap, tap, catchError } from "rxjs/operators";
+import { from, concat, of } from "rxjs";
+import { API_URL } from "../../config/config";
+import { http } from "../../services/http/http";
+import { profilesFetchOneSuccess, profilesFetchOneStart } from "./actions";
+import { ProfilesActions, ProfilesActionType } from "./types";
 
 export const profilesFetchOneEpic = (
   action$: ActionsObservable<ProfilesActions>
@@ -17,5 +17,5 @@ export const profilesFetchOneEpic = (
   );
 
 export const profilesEpics = {
-  profilesFetchOneEpic,
+  profilesFetchOneEpic
 };
